d7b2ab1baeb9128d4c6886bbba2841e63e5ab754
mujo-raw <27100095@lums.edu.pk>
2026-02-17 17:48:11 +0500

    mj

--- app/src/main/java/com/example/mooger/Shape.java
+++ app/src/main/java/com/example/mooger/Shape.java
@@ -3,6 +3,5 @@ package com.example.mooger;
 public abstract class Shape {
     public int x;
     public int y;
-    String color = "red";
-    String color = "yellow";
+    String color = "pink";
 }
